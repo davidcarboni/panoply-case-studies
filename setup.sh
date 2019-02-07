5b5ad0a4a6a2965fab1701fdc7a58b07b32d8240
David Carboni <david@carboni.io>
2019-02-07 15:32:10 +0000

    Setup script to create buckets.

---
+++ setup.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+
+# Prepare
+
+if [ ! -f ./project.txt ]; then
+    echo "Please create a file called project.txt containing your GCP project ID."
+    exit 1
+else
+    BASE=$PWD
+    PROJECT=`cat project.txt`
+fi
+
+
+# Buckets
+
+storage_class=multi_regional
+location=eu
+
+declare -a buckets=(
+  "${PROJECT}-content"
+  "${PROJECT}-frontend"
+)
+
+for bucket in "${buckets[@]}"
+do
+   echo Bucket: $bucket
+   gsutil mb -p $PROJECT -c $storage_class -l $location gs://${bucket}/
+done
+
