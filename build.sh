7be5f431af2476a90560426f568ba34540600a77
David Carboni <david@carboni.io>
2019-02-09 15:46:17 +0000

    Added configurable jwt key and deploying Python version of function.

--- build.sh
+++ build.sh
@@ -11,13 +11,22 @@ else
     PROJECT=`cat project.txt`
 fi
 
+if [ ! -f ./jwt_key.txt ]; then
+    JWT_KEY=justtesting
+else 
+    JWT_KEY=`cat jwt_key.txt
+fi
+
 
 # Update frontend
 gsutil -m rsync -a public-read -J -d -r frontend gs://${PROJECT}-frontend/
 
 
 # Deploy function
-gcloud functions deploy hello --entry-point Hi --runtime go111 --trigger-http --source=./functions/go --region=europe-west1 --update-env-vars PROJECT_ID=${PROJECT}
+#gcloud functions deploy hello --entry-point Hi --runtime go112 --trigger-http --source=./functions/go --region=europe-west1 --update-env-vars PROJECT_ID=${PROJECT}
+
+# Deploy Python version 
+gcloud functions deploy register --entry-point register --runtime python37 --trigger-http --source=./functions/python --region=europe-west1 --update-enw-vars PROJECT_ID=${PROJECT},JWT_KEY=${JWT_KEY}
 
 exit 0
 
