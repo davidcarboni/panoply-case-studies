3ed5580105b7aac91439f6fd49ee5a98e71447d3
David Carboni <david@carboni.io>
2019-02-09 15:40:12 +0000

    Updated Go code, but GCP seems to timeout on deploy, so leaving this for now.

--- functions/go/function.go
+++ functions/go/function.go
@@ -4,11 +4,51 @@ import (
  "net/http"
  "context"
  "fmt"
+ "os"
  "log"
 
  "cloud.google.com/go/datastore"
 )
 
+type CaseStudy struct {
+	OrganisationName string
+	Address string
+	PointOfContact string
+	PositionInOrganisation string
+	Email string
+	Phone string
+	StartDate string
+	CompletionDate string
+	EstimatedValue string
+	ClientRefContactFirst string
+}
+
 func Hi(w http.ResponseWriter, r *http.Request) {
- fmt.Fprintf(w, "Hello, World!")
+	ctx := context.Background()
+	projectID := os.Getenv("PROJECT_ID")
+
+	client, err := datastore.NewClient(ctx, projectID)
+	if err != nil {
+		log.Fatalf("Failed to create client: %v", err)
+	}
+
+	// Sets the kind for the new entity.
+	kind := "CaseStudy"
+	// Sets the name/ID for the new entity.
+	name := "samplecasestudy1"
+	// Creates a Key instance.
+	taskKey := datastore.IncompleteKey(kind, nil)
+
+	// Creates a Task instance.
+	caseStudy := CaseStudy{
+		OrganisationName: "ACME Widget Factory",
+	}
+
+	// Saves the new entity.
+	if _, err := client.Put(ctx, taskKey, &caseStudy); err != nil {
+		log.Fatalf("Failed to save case study: %v", err)
+	}
+
+	fmt.Printf("Saved %v: %v\n", taskKey, caseStudy.OrganisationName)
+	fmt.Fprintf(w, "Saved %v: %v\n", taskKey, caseStudy.OrganisationName)
 }
