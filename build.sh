9d4fd57d501cc665e0e9b0a30f4d77d7a18ad90b
David Carboni <david@carboni.io>
2019-02-09 17:52:29 +0000

    Deploying a new function.

--- build.sh
+++ build.sh
@@ -14,7 +14,7 @@ fi
 if [ ! -f ./jwt_key.txt ]; then
     JWT_KEY=justtesting
 else 
-    JWT_KEY=`cat jwt_key.txt
+    JWT_KEY=`cat jwt_key.txt`
 fi
 
 
@@ -26,7 +26,12 @@ gsutil -m rsync -a public-read -J -d -r frontend gs://${PROJECT}-frontend/
 #gcloud functions deploy hello --entry-point Hi --runtime go112 --trigger-http --source=./functions/go --region=europe-west1 --update-env-vars PROJECT_ID=${PROJECT}
 
 # Deploy Python version 
-gcloud functions deploy register --entry-point register --runtime python37 --trigger-http --source=./functions/python --region=europe-west1 --update-enw-vars PROJECT_ID=${PROJECT},JWT_KEY=${JWT_KEY}
+
+gcloud functions deploy register --entry-point register --runtime python37 --trigger-http --source=./functions/python --region=europe-west1 --update-env-vars PROJECT_ID=${PROJECT},JWT_KEY=${JWT_KEY}
+
+gcloud functions deploy authenticate --entry-point authenticate --runtime python37 --trigger-http --source=./functions/python --region=europe-west1 --update-env-vars PROJECT_ID=${PROJECT},JWT_KEY=${JWT_KEY}
+
+# ---
 
 exit 0
 
