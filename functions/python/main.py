a9f398bab777d387dba804cfa45f50f62c4c2420
David Carboni <david@carboni.io>
2019-02-09 16:36:45 +0000

    Fixed password hashing.

--- functions/python/main.py
+++ functions/python/main.py
@@ -100,15 +100,15 @@ def read(email, client):
 def hash_password(password):
     # See https://github.com/pyca/bcrypt/
     return bcrypt.hashpw(
-        base64.b64encode(hashlib.sha256(password).digest()),
+        base64.b64encode(hashlib.sha256(password.encode('utf-8')).digest()),
         bcrypt.gensalt()
     )
 
 
-def check_password(password):
+def check_password(password, hash):
     # See https://github.com/pyca/bcrypt/
     return bcrypt.checkpw(
-        base64.b64encode(hashlib.sha256(password).digest())
+        base64.b64encode(hashlib.sha256(password.encode('utf-8')).digest()), hash
     )
 
 
